11081c829a737507ae76373732f625a8e553c081
Arozak18 <rozak.a003@gmail.com>
2022-10-13 22:48:07 +0700

    Perbaikan Intent dan pengambilan data dari activity 1 ke activity 2

--- app/src/main/java/com/arstudio/myfirstapp/MyFirstApp2.java
+++ app/src/main/java/com/arstudio/myfirstapp/MyFirstApp2.java
@@ -2,13 +2,23 @@ package com.arstudio.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MyFirstApp2 extends AppCompatActivity {
+    private TextView tampil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_first_app2);
+
+        tampil = findViewById(R.id.tampilkan);
+
+        Intent terima = getIntent();
+        String terimanama = terima.getStringExtra("amplopuser");
+
+        tampil.setText(terimanama);
     }
 }
