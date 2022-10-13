992a1404bf50700235393aa0bb8034396df99fc9
Arozak18 <rozak.a003@gmail.com>
2022-10-13 22:12:07 +0700

    Form login tanpa database

---
+++ app/src/main/java/com/arstudio/myfirstapp/MyFirstApp2.java
@@ -0,0 +1,14 @@
+package com.arstudio.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyFirstApp2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_first_app2);
+    }
+}
